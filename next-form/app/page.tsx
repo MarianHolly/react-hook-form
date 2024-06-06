cf90eedb91fffa4eeb7608e2559edbe9c2dfffcc
Stupel <hellomariandev@gmail.com>
2024-06-06 20:59:27 +0200

    form finished

--- next-form/app/page.tsx
+++ next-form/app/page.tsx
@@ -1,8 +1,14 @@
+import SimpleForm from '@/components/simple'
+import RHF from '@/components/rhf'
+import RHFZod from '@/components/rhf-zod'
+import WithAction from '@/components/with-action'
+
 export default function Home() {
   return (
-    <section className='py-24'>
+    <section className='flex justify-center py-24'>
       <div className='container'>
         <h1 className='mb-16 text-2xl font-medium'>React Hook Forms</h1>
+        <WithAction />
       </div>
     </section>
   )
