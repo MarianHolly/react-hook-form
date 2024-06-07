30fab19998b305f4a5e4d3e2edf19e032e30ce6f
Stupel <hellomariandev@gmail.com>
2024-06-07 10:04:07 +0200

    small conponents

---
+++ multistep-form-redux/components/elements/text-input.jsx
@@ -0,0 +1,34 @@
+export default function TextInput({
+  label,
+  name,
+  register,
+  errors,
+  isRequired = true,
+  type = "text",
+  className = "sm:col-span-2",
+  defaultValue = "",
+}) {
+  return (
+    <div className={className}>
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium leading-6 text-gray-900 dark:text-slate-50 mb-2"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          id={name}
+          name={name}
+          type={type}
+          autoComplete={name}
+          defaultValue={defaultValue}
+          placeholder={`Type the ${label.toLowerCase()}`}
+          {...register(`${name}`, { required: isRequired })}
+          className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-slate-500 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 dark:focus:ring-slate-500 sm:text-sm sm:leading-6 dark:bg-transparent dark:text-slate-100"
+        />
+        {errors[`${name}`] && <span className="text-sm text-red-600 ">{label} is required</span>}
+      </div>
+    </div>
+  );
+}
