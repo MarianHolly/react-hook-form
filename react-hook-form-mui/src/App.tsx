fc3cd10b11f5faa2ca76a6047db0cae4b57d750a
Stupel <hellomariandev@gmail.com>
2024-06-08 09:38:12 +0200

    update

--- react-hook-form-mui/src/App.tsx
+++ react-hook-form-mui/src/App.tsx
@@ -1,7 +1,10 @@
+import Users from "./users/components/users";
+
 export default function App() {
   return (
     <>
       <h1 className="text-center">Hello Vite!</h1>
+      <Users />
     </>
   );
 }
